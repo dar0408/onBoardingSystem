4c619a027e9a36d0e257c6af05cb631320cee546
dar0408 <dsalgotra2021@gmail.com>
2025-05-13 02:10:34 +0530

    [Darpan] add : Add AuditorAwareImpl.java related changes

---
+++ onboarding/src/main/java/com/bridgelabz/onboarding/config/AuditorAwareImpl.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.onboarding.config;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component("auditorAware")
+public class AuditorAwareImpl implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of("system_user");
+    }
+}
