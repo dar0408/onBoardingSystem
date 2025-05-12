c43fec97fcc1377c82aeb6d97fcfcc358a040a6b
dar0408 <dsalgotra2021@gmail.com>
2025-05-13 02:10:36 +0530

    [Darpan] add : Add RabbitMQConfig.java related changes

---
+++ onboarding/src/main/java/com/bridgelabz/onboarding/config/RabbitMQConfig.java
@@ -0,0 +1,40 @@
+package com.bridgelabz.onboarding.config;
+
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.DefaultJackson2JavaTypeMapper;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        Jackson2JsonMessageConverter converter = new Jackson2JsonMessageConverter();
+
+        DefaultJackson2JavaTypeMapper typeMapper = new DefaultJackson2JavaTypeMapper();
+        typeMapper.setTrustedPackages("com.bridgelabz", "java.util");
+
+        converter.setJavaTypeMapper(typeMapper);
+        return converter;
+    }
+
+    @Bean
+    public AmqpTemplate amqpTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(jsonMessageConverter());
+        return template;
+    }
+
+    public static final String JOB_OFFER_QUEUE = "jobOfferQueue";
+
+    @Bean
+    public Queue jobOfferQueue() {
+        return new Queue(JOB_OFFER_QUEUE, true);
+    }
+}
