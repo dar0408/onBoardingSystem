af9f73cf4c8aee9cda5e8aeeae1f628f73093ec6
dar0408 <dsalgotra2021@gmail.com>
2025-05-14 01:52:09 +0530

    [Darpan] add : ForgotPasswordController implementation

---
+++ onboarding/src/main/java/com/bridgelabz/onboarding/controller/ForgotPasswordController.java
@@ -0,0 +1,25 @@
+package com.bridgelabz.onboarding.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.bridgelabz.onboarding.dto.ResetDTO;
+import com.bridgelabz.onboarding.service.PasswordService;
+
+@RestController
+@RequestMapping("/api/forgot-password")
+public class ForgotPasswordController {
+    @Autowired private PasswordService passwordService;
+
+    @PostMapping("/request")
+    public ResponseEntity<?> requestOtp(@RequestParam String email) {
+        passwordService.sendOtp(email);
+        return ResponseEntity.ok("OTP sent");
+    }
+
+    @PostMapping("/reset")
+    public ResponseEntity<?> reset(@RequestBody ResetDTO dto) {
+        passwordService.resetPassword(dto.getEmail(), dto.getOtp(), dto.getNewPassword());
+        return ResponseEntity.ok("Password updated");
+    }
+}
